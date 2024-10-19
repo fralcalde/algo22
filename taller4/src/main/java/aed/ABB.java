ecd71900077c197121c07235bcbc4136348b35fb
Francisco Alcalde <francisco@alcalde.com.ar>
2024-10-18 21:08:33 -0300

    taller4: Avances

--- taller4/src/main/java/aed/ABB.java
+++ taller4/src/main/java/aed/ABB.java
@@ -5,37 +5,96 @@ import java.util.*;
 // Todos los tipos de datos "Comparables" tienen el método compareTo()
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
-    // Agregar atributos privados del Conjunto
+		Nodo _raiz;
+		int _cardinal;
+		Nodo _max;
+		Nodo _min;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
+			Nodo _padre;
+			T _valor;
+			Nodo _izq;
+			Nodo _der;
 
-        // Crear Constructor del nodo
+			Nodo(T elem) {
+				this._padre = null;
+				this._valor = elem;
+				this._izq = null;
+				this._der = null;
+			}
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementada aun");
+			this._raiz = null;
+			this._cardinal = 0;
+			this._max = null;
+			this._min = null;
     }
 
     public int cardinal() {
-        throw new UnsupportedOperationException("No implementada aun");
+			return this._cardinal;
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+			return this._min._valor;
     }
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
+			return this._max._valor;
     }
 
     public void insertar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+			if(_raiz == null) {
+				this._raiz = new Nodo(elem);
+				this._cardinal += 1;
+			} else {
+				abb_insert(null, _raiz, elem);
+			}
     }
 
+		private void abb_insert(Nodo padre, Nodo actual, T elem) {
+			if(actual != null) {
+				if(actual._valor.compareTo(elem) < 0) {
+					abb_insert(actual, actual._der, elem);
+				}
+				if(actual._valor.compareTo(elem) > 0) {
+					abb_insert(actual, actual._izq, elem);
+				}
+			}
+
+			if(actual == null) {
+				Nodo nodo = new Nodo(elem);
+				nodo._padre = padre;
+				this._cardinal += 1;
+
+				if(padre != null) {
+					if(padre._valor.compareTo(elem) < 0) {
+						padre._der = nodo;
+					}
+					if(padre._valor.compareTo(elem) > 0) {
+						padre._izq = nodo;
+					}
+				}
+			}
+		}
+
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+			return abb_in(_raiz, elem);
     }
+
+		private boolean abb_in(Nodo actual, T elem) {
+			boolean res = false;
+			if(actual != null) {
+				if(actual._valor.compareTo(elem) < 0) {
+					res = res || abb_in(actual._izq, elem);
+				}
+				if(actual._valor.compareTo(elem) > 0) {
+					res = res || abb_in(actual._der, elem);
+				}
+			}
+
+			return actual != null && actual._valor.compareTo(elem) == 0;
+		}
 
     public void eliminar(T elem){
         throw new UnsupportedOperationException("No implementada aun");
